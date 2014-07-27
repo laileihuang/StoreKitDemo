34aceabdadfc986ebdfe62609b435b34735e8a5b
Lailei Huang (Alex) <laileihuang@gmail.com>
2014-07-28 08:57:47 +1000

    Initial Commit

---
+++ StoreKitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoreKitDemo
+//
+//  Created by Lailei Huang on 28/07/2014.
+//  Copyright (c) 2014 alex.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StoreKitDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoreKitDemo
+//
+//  Created by Lailei Huang on 28/07/2014.
+//  Copyright (c) 2014 alex.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
